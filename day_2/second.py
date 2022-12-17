b97d138818c8faaf37e33e7bc93b1f52debe8421
lisamazzuoli <lisa.mazzuoli@accenture.com>
2022-12-17 14:44:57 +0100

    day3 of AoC

--- day_2/second.py
+++ day_2/second.py
@@ -1,59 +1,72 @@
 def change_list(lista):
-    Newlista=[]
+    Newlista = []
     for l in lista:
-        if (l=='A') : Newlista.append(1) #potrei usare anche switch
-        elif (l=='B') : Newlista.append(2)
-        elif (l=='C') : Newlista.append(3)
+        if (l == 'A'):
+            Newlista.append(1)  # potrei usare anche switch
+        elif (l == 'B'):
+            Newlista.append(2)
+        elif (l == 'C'):
+            Newlista.append(3)
     return Newlista
+
 
 def change_list1(lista):
-    Newlista=[]
+    Newlista = []
     for l in lista:
-        if (l=='X') : Newlista.append(0) #potrei usare anche switch
-        elif ( l=='Y') : Newlista.append(3)
-        elif (l=='Z') : Newlista.append(6)
+        if (l == 'X'):
+            Newlista.append(0)  # potrei usare anche switch
+        elif (l == 'Y'):
+            Newlista.append(3)
+        elif (l == 'Z'):
+            Newlista.append(6)
     return Newlista
 
 
-def function(lista1,listares,listasua):
-    Newlist=[]
-    pos=0
-    while(len(listares)!=0 and len(listasua)!=0): #posso pure controllarne una sola
-     pos=lista1.index(listasua[0]) #cerco dove sta l'elemento della listasua nella lista 1
-     if(listares[0]==0) : #devo perdere, vado uno indietro
-        if(pos==0) : Newlist.append(lista1[2])
-        else : Newlist.append(lista1[pos-1])
-     if (listares[0]==3) : Newlist.append(lista1[pos]) #devo stare nella sua stessa posizione per pareggiare
-     if (listares[0]==6) :      #devo vincere, vado uno indietro
-        if(pos==2) : Newlist.append(lista1[0])  #non posso andare indietro di uno
-        else : Newlist.append(lista1[pos+1]) #posso andare indietro di 1
-     listares.remove(listares[0])
-     listasua.remove(listasua[0])
-    return Newlist #ritorno una lista fatta da 2,1,0 che descrivono le mie mosse
+def function(lista1, listares, listasua):
+    Newlist = []
+    pos = 0
+    while (len(listares) != 0 and len(listasua) != 0):  # posso pure controllarne una sola
+        pos = lista1.index(listasua[0])  # cerco dove sta l'elemento della listasua nella lista 1
+        if (listares[0] == 0):  # devo perdere, vado uno indietro
+            if (pos == 0):
+                Newlist.append(lista1[2])
+            else:
+                Newlist.append(lista1[pos - 1])
+        if (listares[0] == 3): Newlist.append(lista1[pos])  # devo stare nella sua stessa posizione per pareggiare
+        if (listares[0] == 6):  # devo vincere, vado uno indietro
+            if (pos == 2):
+                Newlist.append(lista1[0])  # non posso andare indietro di uno
+            else:
+                Newlist.append(lista1[pos + 1])  # posso andare indietro di 1
+        listares.remove(listares[0])
+        listasua.remove(listasua[0])
+    return Newlist  # ritorno una lista fatta da 2,1,0 che descrivono le mie mosse
+
 
 def main():
-    listR=[]  #lista risultati
-    listY=[]
-    copia=[]
+    listR = []  # lista risultati
+    listY = []
+    copia = []
     with open("elv2", 'rt', encoding='utf-8') as f:
-      for line in f:
-        listY.append(line[0])
-        listR.append(line[2])
-    list1Y=change_list(listY)
-    list1R=change_list1(listR)
-    #print(list1Y)
-    #print(list1R)
-    for l in list1R : copia.append(l)
-    test=[1,2,3] #lista test
-    sum=0
-    #count=0
-    final_list=function(test,list1R,list1Y) #torno la lista delle mie mosse
-    #print(final_list)
+        for line in f:
+            listY.append(line[0])
+            listR.append(line[2])
+    list1Y = change_list(listY)
+    list1R = change_list1(listR)
+    # print(list1Y)
+    # print(list1R)
+    for l in list1R: copia.append(l)
+    test = [1, 2, 3]  # lista test
+    sum = 0
+    # count=0
+    final_list = function(test, list1R, list1Y)  # torno la lista delle mie mosse
+    # print(final_list)
     for elem in final_list:
-        sum=sum + elem + copia[0]
-        #print('sum=' + str(sum))
+        sum = sum + elem + copia[0]
+        # print('sum=' + str(sum))
         copia.remove(copia[0])
     print('risultato = ' + str(sum))
 
+
 if __name__ == '__main__':
-         main()
+    main()
---
+++ day_3/first.py
@@ -0,0 +1,76 @@
+import string
+from collections import OrderedDict
+
+
+def copy(list):
+    string = []
+    for el in list: string.append(el)
+    return string
+
+
+def intersection(list1, list2):
+    newList = []
+    count = 0
+    clist2 = list2.copy()
+    for element in list1:
+        copia = copy(element)
+        copia1 = copy(clist2[0])  # ho copiato la prima stringa
+        copiares = []
+        copia1res = []
+        [copiares.append(x) for x in copia if x not in copiares]  # devo togliere le ripetizioni
+        [copia1res.append(x) for x in copia1 if x not in copia1res]
+        for el in copiares:
+            if el in copia1res:
+                newList.append(el)
+                count += 1
+        clist2.remove(clist2[0])
+
+    return newList
+
+
+def merge(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
+
+
+def main():
+    list = []
+    list1 = []
+    third = []
+
+    with open("elv3", 'rt', encoding='utf-8') as f:
+        for line in f:
+            third.append(line)
+    for line in third:
+        list.append(line[:len(line) // 2])
+        list1.append(line[len(line) // 2:])
+
+    common_list = []  # lista degli elementi in comune senza ripetizioni
+    copylist = list.copy()
+    copylist1 = list1.copy()
+    common_list = intersection(copylist, copylist1)
+
+    dict = {}
+    dict = OrderedDict.fromkeys(string.ascii_lowercase, range(0))
+    i = 1
+    for k, v in dict.items():
+        dict[k] = i
+        i += 1
+    dict2 = OrderedDict.fromkeys(string.ascii_uppercase, range(0))
+    j = 27
+    for k, v in dict2.items():
+        dict2[k] = j
+        j += 1
+    mergeddict = {}
+    mergeddict = merge(dict, dict2)  # dict con key=lettere e values=numeri
+
+    sum = 0
+
+    for elem in common_list: sum = sum + int(mergeddict[elem])  # cerco l'elemento della common list nelle keys
+                                                                # e rimando il relativo value
+    print('sum=' + str(sum))
+
+
+if __name__ == '__main__':
+    main()
