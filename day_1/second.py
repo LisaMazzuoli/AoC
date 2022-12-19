1f1d488eb0b97e06a82825aa1d36edc0ef77c09a
d3reo <dario.balestri@accenture.com>
2022-12-19 10:16:02 +0100

    Again, quoting the ZoP, "Explicit is better than implicit".
    Also, some words are keywords to the language, so it's best not to use those as variable names (e.g. "sum").

--- day_1/second.py
+++ day_1/second.py
@@ -2,14 +2,14 @@ from first import parse_elves_from_problem_file
 
 
 def main():
-    d = parse_elves_from_problem_file()
-    sum = 0
-    sum += d[max(zip(d.values(), d.keys()))[1]]
-    d[max(zip(d.values(), d.keys()))[1]] = 0
-    sum += d[max(zip(d.values(), d.keys()))[1]]
-    d[max(zip(d.values(), d.keys()))[1]] = 0
-    sum += d[max(zip(d.values(), d.keys()))[1]]
-    print('la somma delle calorie dei primi tre è ' + str(sum))
+    elves_dict = parse_elves_from_problem_file()
+    top_three_tot_calories = 0
+    top_three_tot_calories += elves_dict[max(zip(elves_dict.values(), elves_dict.keys()))[1]]
+    elves_dict[max(zip(elves_dict.values(), elves_dict.keys()))[1]] = 0
+    top_three_tot_calories += elves_dict[max(zip(elves_dict.values(), elves_dict.keys()))[1]]
+    elves_dict[max(zip(elves_dict.values(), elves_dict.keys()))[1]] = 0
+    top_three_tot_calories += elves_dict[max(zip(elves_dict.values(), elves_dict.keys()))[1]]
+    print('la somma delle calorie dei primi tre è ' + str(top_three_tot_calories))
 
 
 if __name__ == '__main__':
