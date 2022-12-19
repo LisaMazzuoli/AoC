59a793c84840541cade27e9385550618978afc7e
d3reo <dario.balestri@accenture.com>
2022-12-19 10:40:59 +0100

    This is the first solution that came to my mind, but it has a different time complexity than the one you proposed, so I'm leaving both of them. Let's discuss pro's and con's of the two.

--- day_1/second.py
+++ day_1/second.py
@@ -3,12 +3,16 @@ from first import parse_elves_from_problem_file
 
 def main():
     elves_dict = parse_elves_from_problem_file()
-    top_n_tot_calories = 0
     top_calories_number = 3
-    for _ in range(top_calories_number):
-        top_scoring_elf = max(elves_dict, key=elves_dict.get)
-        top_n_tot_calories += elves_dict[top_scoring_elf]
-        del elves_dict[top_scoring_elf]
+
+    descending_sorted_calories = sorted(elves_dict.values(), reverse=True)
+    top_n_tot_calories = sum(descending_sorted_calories[:3])
+
+    # top_n_tot_calories = 0
+    # for _ in range(top_calories_number):
+    #     top_scoring_elf = max(elves_dict, key=elves_dict.get)
+    #     top_n_tot_calories += elves_dict[top_scoring_elf]
+    #     del elves_dict[top_scoring_elf]
 
     print('la somma delle calorie dei primi tre è ' + str(top_n_tot_calories))
 
